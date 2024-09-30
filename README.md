cec3dee7869d5bb54bb2ba240a9a7a6e39b89bcc
BW <17097491+bw-01@users.noreply.github.com>
2024-09-30 16:51:15 +1300

    Basic game functionality is working in the console

---
+++ README.md
@@ -0,0 +1,13 @@
+# Tic Tac Toe Website
+
+A JavaScript Tic Tac Toe game. The primary goal of this project is to demonstrate knowledge of how to use factory functions and IIFEs to organise code and minimise the amount of global code.
+
+- Author: Bart W
+
+## Installation
+
+To set up the project locally:
+
+1. Clone the repository
+2. Navigate to the project directory
+3. Open `index.html` in your browser to view the website.
---
+++ main.js
@@ -0,0 +1,115 @@
+// IIFE for the Gameboard
+const Gameboard = (function () {
+  let boardState = Array(9).fill(" ");
+
+  const getBoard = () => boardState;
+
+  const updateBoard = (index, playerToken) => {
+    if (boardState[index] != " ") {
+      console.log("Cell already occupied.");
+      return false;
+    } else {
+      boardState[index] = playerToken;
+      return true;
+    }
+  };
+
+  const boardFull = () => {
+    return !boardState.includes(" ");
+  };
+
+  // Print the board state to the console
+  const printBoard = () => {
+    console.log(boardState.slice(0, 3));
+    console.log(boardState.slice(3, 6));
+    console.log(boardState.slice(6, 9));
+  };
+
+  const resetBoard = () => {
+    boardState = Array(9).fill(" ");
+  };
+
+  return { getBoard, updateBoard, boardFull, printBoard, resetBoard };
+})();
+
+// Factory function to create the players
+function Player(name, token) {
+  return { name, token };
+}
+
+// Function to control the flow of the game
+function GameController() {
+  const player1 = Player("Bob", "x");
+  const player2 = Player("Sue", "o");
+  const winningLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  let gameover = false;
+  let activePlayer = player1;
+
+  const switchActivePlayer = () => {
+    activePlayer = activePlayer === player1 ? player2 : player1;
+  };
+
+  const checkGameEnd = (activePlayer) => {
+    const board = Gameboard.getBoard();
+
+    for (let i = 0; i < winningLines.length; i++) {
+      const line = winningLines[i];
+      let playerWon = true;
+
+      for (let j = 0; j < line.length; j++) {
+        const boardIndex = line[j];
+
+        if (board[boardIndex] != activePlayer.token) {
+          playerWon = false;
+          break;
+        }
+      }
+
+      if (playerWon) {
+        console.log(activePlayer.name + " wins!!!");
+        gameover = true;
+      }
+    }
+
+    if (!gameover && Gameboard.boardFull()) {
+      console.log("It's a draw!");
+      gameover = true;
+    }
+  };
+
+  const playRound = () => {
+    while (!gameover) {
+      let validChoice = false;
+      let playerChoice = prompt(activePlayer.name + " please pick a cell from 1 to 9");
+      let index = parseInt(playerChoice) - 1;
+      while (index > 8 || index < 0 || isNaN(index)) {
+        playerChoice = prompt("Invalid input. Please pick a cell from 1 to 9 ");
+        index = parseInt(playerChoice) - 1;
+      }
+
+      validChoice = Gameboard.updateBoard(index, activePlayer.token);
+
+      if (validChoice) {
+        Gameboard.printBoard();
+        checkGameEnd(activePlayer);
+        switchActivePlayer();
+      }
+    }
+  };
+
+  playRound();
+
+  return {};
+}
+
+const game = GameController();
